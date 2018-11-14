db92abe41f233cafa37c745a02db04984368fa1b
dnnxl15 <dnnxl15@hotmail.com>
2018-11-14 16:36:47 -0600

    Agregar archivos de proyecto.

---
+++ WebServiceDesign/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebServiceDesign
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
